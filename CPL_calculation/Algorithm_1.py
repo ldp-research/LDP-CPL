c57398644b05ff0c0420819d12a3c9c310402c00
ldp-research <swjresearch@gmail.com>
2025-08-28 07:11:12 +1000

    Update Algorithm_1.py

--- CPL_calculation/Algorithm_1.py
+++ CPL_calculation/Algorithm_1.py
@@ -2,6 +2,10 @@
     Algorithm 1 
     (Actual CPL using transition probability distribution of LDP mechanism 
     and proabability distribution between attributes).
+
+    p - transition probabilities of LDP mechanism.
+    CMF - conditional probability distribution between attributes.
+    eps - privacy budget
 '''
 
 import numpy as np
@@ -9,14 +13,14 @@ import numpy as np
 def compute_leakage(C, G, G_prime):
     return np.sum(C*G)/np.sum(C*G_prime)
 
-def Algo3_privacy_leakage(Q, CMF, eps):
+def algo1(p, CMF, eps): 
     
     num_rows, _ = np.shape(CMF)
-    _, num_columns_Q = np.shape(Q)
+    _, num_columns_p = np.shape(p)
     l = 0
     
-    for k in range(num_columns_Q):
-        C = Q[:,k]
+    for k in range(num_columns_p):
+        C = p[:,k]
         for i in range(num_rows):
             G = CMF[i,:]
             for j in range(num_rows):
